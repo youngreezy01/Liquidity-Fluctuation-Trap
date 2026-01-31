e6721ae206952955c3577f566cdb69f9ce76e5bd
youngreezy01 <140664698+youngreezy01@users.noreply.github.com>
2026-01-31 21:40:55 +0100

    Update Deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import {Script} from "@forge-std/Script.sol";
+import {SimpleResponder} from "../src/SimpleResponder.sol";
+import {LiquidityFluctuationTrap} from "../src/LiquidityFluctuationTrap.sol";
+
+contract DeployScript is Script {
+    function run() public {
+        vm.startBroadcast();
+        
+        SimpleResponder responder = new SimpleResponder();
+        console.log("SimpleResponder:", address(responder));
+        
+        LiquidityFluctuationTrap trap = new LiquidityFluctuationTrap();
+        console.log("LiquidityFluctuationTrap:", address(trap));
+        
+        vm.stopBroadcast();
+    }
+}
