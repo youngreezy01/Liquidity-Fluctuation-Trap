f89f3b2b19c390e6b7278201205956a3e83d4738
youngreezy01 <140664698+youngreezy01@users.noreply.github.com>
2026-01-31 21:40:30 +0100

    Update SimpleResponder.sol

--- src/SimpleResponder.sol
+++ src/SimpleResponder.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract SimpleResponder {
+    event ResponseTriggered(uint256 liquidityAmount, address caller, uint256 timestamp);
+    
+    function respondCallback(uint256 amount) public {
+        emit ResponseTriggered(amount, msg.sender, block.timestamp);
+    }
+}
